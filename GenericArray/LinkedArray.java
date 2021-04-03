2d373fa5444f105a753cec46133bd850c0fdd395
Rafael Malon <33752131+r-malon@users.noreply.github.com>
2021-04-03 18:34:42 -0300

    exercise done

--- GenericArray/LinkedArray.java
+++ GenericArray/LinkedArray.java
@@ -28,29 +28,132 @@ public class LinkedArray<T> implements GenericArray<T> {
 		counter++;
 	}
 
-	public void add(int pos, T element) {}
+	public void add(int pos, T element) {
+		Node node = new Node(element, null);
+		Node curr = first;
+		Node prev = null;
+		Node next = null;
+
+		if (pos == 0) {
+			node.setNext(first);
+			first = node;
+		} else {
+			for (int i = 0; i < counter; i++) {
+				if (i == (pos - 1))
+					prev = curr;
+				else if (i == pos) {
+					next = curr;
+					break;
+				}
+				curr = curr.getNext();
+			}
+			prev.setNext(node);
+			node.setNext(next);
+		}
+		counter++;
+	}
 
 	public T remove(int pos) {
 		T removed = null;
 		Node curr = first;
-		Node prev;
+		Node prev = null;
+		Node next = null;
 
-		for (int i = 0; i < counter; i++) {
-			if (i == (pos - 1)) {
-				prev = curr;
+		if (pos == 0) {
+			removed = (T) first.getData();
+			first = first.getNext();
+		} else {
+			for (int i = 0; i < counter; i++) {
+				if (i == (pos - 1))
+					prev = curr;
+				else if (i == pos)
+					removed = (T) curr.getData();
+				else if (i == (pos + 1)) {
+					next = curr;
+					break;
+				}
+				curr = curr.getNext();
 			}
-			curr = curr.getNext();
+			prev.setNext(next);
 		}
+		counter--;
 		return removed;
 	}
 
-	public boolean removeFirst(T element) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
-	public T get(int pos) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
-	public T set(int pos, T element) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+	public boolean removeFirst(T element) {
+		Node curr = first;
 
-	public int indexOf(T element) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
-	public int lastIndexOf(T element) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
-	public boolean contains(T element) {throw new java.lang.UnsupportedOperationException("Not supported yet.");}
+		for (int i = 0; i < counter; i++) {
+			if ((T) curr.getData() == element) {
+				remove(i);
+				return true;
+			}
+			curr = curr.getNext();
+		}
+		return false;
+	}
+
+	public T get(int pos) {
+		Node curr = first;
+
+		for (int i = 0; i < counter; i++) {
+			if (i == pos) {
+				return (T) curr.getData();
+			}
+			curr = curr.getNext();
+		}
+		return null;
+	}
+
+	public T set(int pos, T element) {
+		Node curr = first;
+		T old = null;
+
+		for (int i = 0; i < counter; i++) {
+			if (i == pos) {
+				old = (T) curr.getData();
+				curr.setData(element);
+				break;
+			}
+			curr = curr.getNext();
+		}
+		return old;
+	}
+
+	public int indexOf(T element) {
+		Node curr = first;
+
+		for (int i = 0; i < counter; i++) {
+			if ((T) curr.getData() == element) {
+				return i;
+			}
+			curr = curr.getNext();
+		}
+		return -1;
+	}
+
+	public int lastIndexOf(T element) {
+		Node curr = first;
+		int index = -1;
+
+		for (int i = 0; i < counter; i++) {
+			if ((T) curr.getData() == element) {
+				index = i;
+			}
+			curr = curr.getNext();
+		}
+		return index;
+	}
+
+	public boolean contains(T element) {
+		Node curr = first;
+
+		for (int i = 0; i < counter; i++) {
+			if ((T) curr.getData() == element) return true;
+			curr = curr.getNext();
+		}
+		return false;
+	}
 
 	public boolean isEmpty() {
 		return counter == 0;
--- GenericArray/Main.java
+++ GenericArray/Main.java
@@ -6,8 +6,11 @@ public class Main {
 		arr.add("abc");
 		arr.add("xx");
 		arr.add("nn");
+		arr.add(2, "ff");
 		arr.add("ff");
-		System.out.printf("Removed: %s%n", arr.remove(2));
+		System.out.printf("lastIndexOf: %s%n", arr.lastIndexOf("ff"));
+		System.out.printf("Removed: %s%n", arr.removeFirst("ff"));
+		System.out.printf("Removed: %s%n", arr.remove(0));
 		System.out.println(arr.size());
 		for (var element : arr.toArray()) {
 			System.out.println(element);
