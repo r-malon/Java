eb722f4478d5605d6251dbb75d246edbb3c35f33
Rafael Malon <33752131+r-malon@users.noreply.github.com>
2021-05-23 20:57:28 -0300

    Finishing Stack

--- Stack/LinkedStack.java
+++ Stack/LinkedStack.java
@@ -7,6 +7,30 @@ public class LinkedStack<T> implements GenericStack<T> {
 		this.top = null;
 	}
 
+	public void push(T element) throws Exception {
+		Node node = new Node(element, null);
+		if (top == null) {
+			top = node;
+		} else {
+			node.setNext(top);
+			top = node;
+		}
+	}
+
+	public T pop() throws Exception {
+		if (top == null)
+			throw new Exception("Empty stack");
+		T removed = top.getData();
+		top = top.getNext();
+		return removed;
+	}
+
+	public T top() throws Exception {
+		if (top == null)
+			throw new Exception("Empty stack");
+		return top.getData();
+	}
+
 	public void clear() {
 		top = null;
 	}
